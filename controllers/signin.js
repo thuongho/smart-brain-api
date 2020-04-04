37682296171ab4c6a74d738c48449e4485850a31
Thuong Ho <thuong.t.ho@gmail.com>
2020-04-04 13:28:29 -0700

    add set token method to save jwt into redis

--- controllers/signin.js
+++ controllers/signin.js
@@ -34,11 +34,19 @@ const signToken = (email) => {
   return jwt.sign(jwtPayload, process.env.JWTSECRET, { expiresIn: '2 days' });
 };
 
+const setToken = (key, value) => {
+  return Promise.resolve(redisClient.set(key, value));
+};
+
 const createSessions = (user) => {
   // create JWT and return user data
   const { email, id } = user;
   const token = signToken(email);
-  return { success: 'true', userId: id, token };
+  return setToken(token, id)
+    .then(() => {
+      return { success: 'true', userId: id, token };
+    })
+    .catch((err) => console.log)
 };
 
 const signinAuthentication = (db, bcrypt) => (req, res) => {
