eba471914eb4a13f87ba01996cc1fecaae14f1ce
Thuong Ho <thuong.t.ho@gmail.com>
2020-04-03 17:19:57 -0700

    update ez login for seed

--- postgres/seed/seed.sql
+++ postgres/seed/seed.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-INSERT into users (name, email, entries, joined) values ('Jessie', 'jessie@gmail.com', 5, '2020-01-25');
-INSERT into login (hash, email) values ('$2a$10$mXGNv6tzFP.WfghUfy/08ujGySORWO7YXhoL8l.d6VIRVOyKBymDe', 'jessie@gmail.com');
+INSERT into users (name, email, entries, joined) values ('Jessie', 'j@gmail.com', 5, '2020-01-25');
+INSERT into login (hash, email) values ('$2a$10$hV0AlUVEtoJHMk6Jcz4Oa.51dNepztfRNcAe1Dr42nLvIudRcVmgW', 'j@gmail.com');
 
 COMMIT;
