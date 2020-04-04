d4b68a2961e67c2f187da4ece6f86e7f6d3c4771
Thuong Ho <thuong.t.ho@gmail.com>
2020-04-03 20:01:52 -0700

    add jsonwebtoken to create session with sigin

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,3 +1,5 @@
+var jwt = require('jsonwebtoken');
+
 const handleSignin = (db, bcrypt, req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
@@ -23,11 +25,29 @@ const getAuthTokenId = () => {
   console.log('getting Auth token');
 };
 
+const signToken = (email) => {
+  const jwtPayload = { email };
+  return jwt.sign(jwtPayload, process.env.JWTSECRET, { expiresIn: '2 days' });
+};
+
+const createSessions = (user) => {
+  // create JWT and return user data
+  const { email, id } = user;
+  const token = signToken(email);
+  return { success: 'true', userId: id, token };
+};
+
 const signinAuthentication = (db, bcrypt) => (req, res) => {
   const { authorization } = req.headers;
   return authorization ? getAuthTokenId() :
     handleSignin(db, bcrypt, req, res)
-      .then(data => res.json(data))
+      .then(data => {
+        return data.id && data.email ?
+          createSessions(data) :
+          // debug with data but should be a message instead
+          Promise.reject(data);
+      })
+      .then(session => res.json(session))
       .catch(err => res.status(400).json(err))
 };
 
